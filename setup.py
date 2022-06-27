3bd571c6c4923b28acb805708edcb9f36e99c037
chloevogel <72452570+chloevogel@users.noreply.github.com>
2022-06-27 15:27:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ from test_functions import __version__
 
 
 setup(name='test_functions',
-version= __version__,
-description='Testing the computation of OTscOmics',
-url='https://github.com/chloevogel/Test_OTscOmics',
-author='Chloé Vogel',
-author_email='chloe.vogel@etu.minesparis.psl.eu',
-packages=['test_functions'],
-zip_safe=False)
+      version= __version__,
+      description='Testing the computation of OTscOmics',
+      url='https://github.com/chloevogel/Test_OTscOmics',
+      author='Chloé Vogel',
+      author_email='chloe.vogel@etu.minesparis.psl.eu',
+      packages=['test_functions'],
+      install_requires=[
+        'pot>=0.8',
+        'torch>=1.0',
+        'numpy>=1.20',
+        'scipy>=1.6',
+        'tqdm>=4.62'
+      ]
+     )
