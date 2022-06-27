7e10fd5ffff23d9ca743a3743d8e59c2f481b4db
chloe.vogel <chloe.vogel@mines-paristech.fr>
2022-06-27 15:08:46 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='test_functions',
+version='0.1',
+description='Testing the computation of OTscOmics',
+url='https://github.com/chloevogel/Test_OTscOmics',
+author='Chloé Vogel',
+author_email='chloe.vogel@etu.minesparis.psl.eu',
+packages=['test_functions'],
+zip_safe=False)
